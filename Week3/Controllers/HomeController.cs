14c878f907017b1d4f6d2a5c6768d01666277214
chon0010 <chon0010@student.monash.edu>
2023-08-20 19:57:23 +1000

    添加项目文件。

---
+++ Week3/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week3.HelloWorld;
+
+namespace Week3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            Hello hello = new Hello();
+            ViewBag.Message = hello.GetHello();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
